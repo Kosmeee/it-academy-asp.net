687e741b20897a35602c253d6f16482c5272f117
Kosmeee <lalll@tut.by>
2020-10-16 01:49:13 +0300

    created project

---
+++ ItAcademy.Project.Music.Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ItAcademy.Project.Music.Client.MvcApplication" Language="C#" %>
---
+++ ItAcademy.Project.Music.Domain/Models/Artist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItAcademy.Project.Music.Domain.Models
+{
+    public class Artist
+    {
+    }
+}
